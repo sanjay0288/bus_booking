cb4e857b3dace9685b3d95a91f5f9168edfe57f5
sanjay0288 <87768165+sanjay0288@users.noreply.github.com>
2024-01-23 19:05:09 +0530

    Update AppConfig.java

--- src/main/java/com/example/config/AppConfig.java
+++ src/main/java/com/example/config/AppConfig.java
@@ -1,1 +1,12 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public BusBookingService busBookingService() {
+        return new BusBookingService();
+    }
+}
 
