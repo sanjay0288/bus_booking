0a85f7987a0c066b0347d5b535b111d7cf369f15
sanjay0288 <87768165+sanjay0288@users.noreply.github.com>
2024-01-23 19:06:14 +0530

    Update BusBookingApplication.java

--- src/main/java/com/example/BusBookingApplication.java
+++ src/main/java/com/example/BusBookingApplication.java
@@ -2,12 +2,17 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BusBookingApplication {
+public class BusBookingApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BusBookingApplication.class, args);
     }
-}
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BusBookingApplication.class);
+    }
+}
